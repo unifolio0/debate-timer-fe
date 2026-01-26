208b6972c80369f2fbf8b998d79223ea6b6f26a5
Shawn Kang <csm06063@naver.com>
2026-01-27 03:15:46 +0900

    test: Story 변수 값 수정

--- src/components/UpdateModal/UpdateModal.stories.tsx
+++ src/components/UpdateModal/UpdateModal.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
-import UpdateModal from './UpdateModal';
+import UpdateModal from './UpdateModal'; // 이미지 임포트는 아래와 같이
+import PatchNoteImage from '../assets/patchNote/0001.png';
 
 const meta: Meta<typeof UpdateModal> = {
   title: 'components/UpdateModal',
@@ -13,8 +14,15 @@ type Story = StoryObj<typeof UpdateModal>;
 
 export const Default: Story = {
   args: {
-    title: '피드백 & 투표',
-    description:
-      '토론 종료 후 피드백 & 투표 기능으로\n다양한 서비스를 이용하세요!',
+    data: {
+      version: '0000',
+      title: '피드백 & 투표',
+      description:
+        '토론 종료 후 피드백 & 투표 기능으로\n다양한 서비스를 이용하세요!',
+      link: 'https://notion.so/',
+      image: PatchNoteImage,
+    },
+    isChecked: false,
+    onChecked: () => {},
   },
 };
