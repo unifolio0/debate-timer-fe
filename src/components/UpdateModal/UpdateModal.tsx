8a5f87ec108821d83584169bc2335a0ecaa6b0fe
Shawn Kang <csm06063@naver.com>
2026-01-28 01:24:17 +0900

    feat: UI에 ImageOnlyPatchNoteData 변경 사항 반영

--- src/components/UpdateModal/UpdateModal.tsx
+++ src/components/UpdateModal/UpdateModal.tsx
@@ -1,6 +1,11 @@
 import MegaphoneAsset from './MegaphoneAsset';
 import NoticeAsset from './NoticeAsset';
-import { PatchNoteData } from '../../constants/patch_note';
+import {
+  ImageOnlyPatchNoteData,
+  isPredefinedPatchNote,
+  PatchNoteData,
+  PredefinedPatchNoteData,
+} from '../../constants/patch_note';
 
 interface UpdateModalProps {
   data: PatchNoteData;
@@ -22,49 +27,79 @@ export default function UpdateModal({
 
   return (
     <div className="flex size-[1000px] flex-col rounded-[22px] bg-default-white">
-      {/* 메인 컨텐츠 */}
-      <div className="flex h-[600px] w-full flex-col space-y-[20px] bg-[#EFF0F4] p-[45px]">
-        <div className="flex w-full flex-row items-center space-x-[10px]">
-          <MegaphoneAsset className="w-[107px]" />
-          <div className="flex flex-col space-y-[18px]">
-            <p className="font-[20px] leading-none text-default-black">
-              디베이트 타이머에 새로운 기능이 생겼어요!
-            </p>
-            <NoticeAsset />
-          </div>
-        </div>
+      {isPredefinedPatchNote(data) ? (
+        <>
+          {/* 메인 컨텐츠 */}
+          <div className="flex h-[600px] w-full flex-col space-y-[20px] bg-[#EFF0F4] p-[45px]">
+            <div className="flex w-full flex-row items-center space-x-[10px]">
+              <MegaphoneAsset className="w-[107px]" />
+              <div className="flex flex-col space-y-[18px]">
+                <p className="font-[20px] leading-none text-default-black">
+                  디베이트 타이머에 새로운 기능이 생겼어요!
+                </p>
+                <NoticeAsset />
+              </div>
+            </div>
 
-        <div className="flex w-full flex-1">
-          {data.image && (
+            <div className="flex w-full flex-1">
+              {data.image && (
+                <img
+                  src={data.image}
+                  alt="업데이트 이미지"
+                  className="h-full w-full rounded-lg object-contain"
+                />
+              )}
+            </div>
+          </div>
+
+          {/* 텍스트 컨텐츠 */}
+          <div className="flex w-full flex-1 flex-col p-[8px]">
+            {/* 타이틀 및 내용 */}
+            <div className="flex h-full flex-col items-center justify-center">
+              <p className="text-[45px] font-bold text-brand">
+                {(data as PredefinedPatchNoteData).title}
+              </p>
+              <div className="mb-[16px] mt-[8px] h-[2px] w-[100px] bg-brand" />
+              <p className="text-center text-[24px]">
+                {(data as PredefinedPatchNoteData).description}
+              </p>
+            </div>
+
+            {/* '일주일 간 보지 않기' 체크박스 */}
+            <div className="flex w-full flex-row items-center space-x-[8px]">
+              <input
+                type="checkbox"
+                className="border-gray size-[24px] rounded-[4px]"
+                checked={isChecked}
+                onChange={() => onChecked(!isChecked)}
+              />
+              <p>일주일 간 보지 않기</p>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="relative flex h-full w-full flex-1">
+          {/* 이미지 컨텐츠 */}
+          {(data as ImageOnlyPatchNoteData).image && (
             <img
               src={data.image}
               alt="업데이트 이미지"
-              className="h-full w-full rounded-lg object-contain"
+              className="h-full w-full rounded-t-lg object-contain"
             />
           )}
-        </div>
-      </div>
 
-      {/* 텍스트 컨텐츠 */}
-      <div className="flex w-full flex-1 flex-col p-[8px]">
-        {/* 타이틀 및 내용 */}
-        <div className="flex h-full flex-col items-center justify-center">
-          <p className="text-[45px] font-bold text-brand">{data.title}</p>
-          <div className="mb-[16px] mt-[8px] h-[2px] w-[100px] bg-brand" />
-          <p className="text-center text-[24px]">{data.description}</p>
+          {/* '일주일 간 보지 않기' 체크박스 */}
+          <div className="absolute bottom-[8px] start-[8px] flex w-full flex-row items-center space-x-[8px]">
+            <input
+              type="checkbox"
+              className="border-gray size-[24px] rounded-[4px]"
+              checked={isChecked}
+              onChange={() => onChecked(!isChecked)}
+            />
+            <p>일주일 간 보지 않기</p>
+          </div>
         </div>
-
-        {/* '일주일 간 보지 않기' 체크박스 */}
-        <div className="flex w-full flex-row items-center space-x-[8px]">
-          <input
-            type="checkbox"
-            className="border-gray size-[24px] rounded-[4px]"
-            checked={isChecked}
-            onChange={() => onChecked(!isChecked)}
-          />
-          <p>일주일 간 보지 않기</p>
-        </div>
-      </div>
+      )}
 
       {/* 버튼 영역 */}
       <button
