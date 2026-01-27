088b6a2e16b96dab9b59eb2e668e795bdff7ed5b
Shawn Kang <csm06063@naver.com>
2026-01-28 01:36:45 +0900

    test: Story에 확장된 타입 명시

--- src/components/UpdateModal/UpdateModal.stories.tsx
+++ src/components/UpdateModal/UpdateModal.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react';
 import UpdateModal from './UpdateModal'; // 이미지 임포트는 아래와 같이
 import PatchNoteImage from '../assets/patchNote/0001.png';
+import { PredefinedPatchNoteData } from '../../constants/patch_note';
 
 const meta: Meta<typeof UpdateModal> = {
   title: 'components/UpdateModal',
@@ -21,7 +22,7 @@ export const Default: Story = {
         '토론 종료 후 피드백 & 투표 기능으로\n다양한 서비스를 이용하세요!',
       link: 'https://notion.so/',
       image: PatchNoteImage,
-    },
+    } as PredefinedPatchNoteData,
     isChecked: false,
     onChecked: () => {},
   },
