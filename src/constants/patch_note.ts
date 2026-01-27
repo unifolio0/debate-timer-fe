a558380a340878a4bd80f6580fc79b6a77f0b934
Shawn Kang <csm06063@naver.com>
2026-01-28 01:23:59 +0900

    feat: 단일 이미지만 표시하는 패치 노트 타입 추가

--- src/constants/patch_note.ts
+++ src/constants/patch_note.ts
@@ -1,16 +1,37 @@
 // 이미지 임포트는 아래와 같이
 import PatchNoteImage from '../assets/patchNote/0001.png';
 
+// 기본적인 패치 노트 인터페이스
 export interface PatchNoteData {
   version: string; // 로컬 스토리지 키 관리를 위한 버전 (이 버전을 바꾸면 사용자의 '다시 보지 않기'가 초기화됨)
-  title: string; // 패치 노트 제목
-  description: string; // 패치 노트 설명
-  image: string; // 이미지
-  link: string; // 외부 공지 링크 (Notion 등)
+  link: string;
+  image: string;
+  mode: 'predefined' | 'image-only';
+}
+
+// 사전 정의된 패치 노트 인터페이스
+export interface PredefinedPatchNoteData extends PatchNoteData {
+  mode: 'predefined';
+  title: string;
+  description: string;
+}
+
+// 이미지만 존재하는 패치 노트 인터페이스
+export interface ImageOnlyPatchNoteData extends PatchNoteData {
+  mode: 'image-only';
+}
+
+// PatchNoteData 타입이 PredefinedPatchNoteData인지 ImagePatchNoteData인지 구별하는 함수
+export function isPredefinedPatchNote(
+  data: PatchNoteData,
+): data is PredefinedPatchNoteData {
+  // 'title' 속성이 data 객체 안에 있는지 확인
+  return data.mode === 'predefined';
 }
 
 // 현재 활성화된 업데이트 데이터 (이 부분만 수정해서 배포하면 됨)
-export const LATEST_PATCH_NOTE: PatchNoteData = {
+export const LATEST_PATCH_NOTE: PredefinedPatchNoteData = {
+  mode: 'predefined',
   version: '0001',
   title: '피드백 & 투표',
   description:
@@ -18,3 +39,13 @@ export const LATEST_PATCH_NOTE: PatchNoteData = {
   image: PatchNoteImage,
   link: 'https://bustling-bathtub-b3a.notion.site/2f41550c60cf80f69227e3145f6e19cc?pvs=143',
 };
+
+// ImageOnlyPatchNoteData의 예시
+/*
+export const TEST_PATCH_NOTE: ImageOnlyPatchNoteData = {
+  mode: 'image-only',
+  version: '0001',
+  image: PatchNoteImage,
+  link: 'https://bustling-bathtub-b3a.notion.site/2f41550c60cf80f69227e3145f6e19cc?pvs=143',
+};
+*/
