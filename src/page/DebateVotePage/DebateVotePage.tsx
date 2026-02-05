ae4fd85e518ca66fb59ad294d4df044b50739322
unifolio0 <boho82ha@gmail.com>
2026-02-05 15:47:42 +0900

    test

--- src/page/DebateVotePage/DebateVotePage.tsx
+++ src/page/DebateVotePage/DebateVotePage.tsx
@@ -23,7 +23,9 @@ export default function DebateVotePage() {
   const isArgsValid = isPollIdValid && isTableIdValid;
 
   const voteUrl = useMemo(() => {
-    return `${baseUrl}/vote/${pollId}`;
+    const basePath = import.meta.env.VITE_BASE_PATH;
+    const pathPrefix = basePath && basePath !== '/' ? basePath : '';
+    return `${baseUrl}${pathPrefix}/vote/${pollId}`;
   }, [baseUrl, pollId]);
 
   const handleGoToResult = () => {
